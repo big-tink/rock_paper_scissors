4bf1133426645fca959e156f5897aacdb3da5969
Jake Bellm <[jbsnake13@gmail.com]>
2024-08-12 13:45:13 -0500

    Build round play and fix human lowercasing

--- myscripts.js
+++ myscripts.js
@@ -6,20 +6,62 @@ let computerScore = 0;
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
         if (computerChoice === 1) {
-            computerChoice = "rock"
+            computerChoice = "rock"      
         } else if (computerChoice === 2) {
             computerChoice = "paper"
         } else {
             computerChoice = "scissors"
         }
+        return computerChoice;
 }
 
 function getHumanChoice() {
     let humanChoice = prompt("Please type rock, paper or scissors:");
-    humanChoice;
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice;
 }
 
-function playRound(computerChoice, humanChoice)  {
-    getHumanChoice();
-    getComputerChoice();
+function playRound()  {
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+
+    if (computerSelection === "rock" && humanSelection === "paper") {
+        console.log("You have selected " + humanSelection + ". Computer has selected " + computerSelection + ".")
+        console.log("Congrats! You win this round!")
+        console.log("Your score is " + ++humanScore + ".")
+        console.log("Computer score is " + computerScore + ".")
+    } else if (computerSelection === "paper" && humanSelection === "scissors") {
+        console.log("You have selected " + humanSelection + ". Computer has selected " + computerSelection + ".")
+        console.log("Congrats! You win this round!")
+        console.log("Your score is " + ++humanScore + ".")
+        console.log("Computer score is " + computerScore + ".")
+    } else if (computerSelection === "scissors" && humanSelection === "rock") {
+        console.log("You have selected " + humanSelection + ". Computer has selected " + computerSelection + ".")
+        console.log("Congrats! You win this round!")
+        console.log("Your score is " + ++humanScore + ".")
+        console.log("Computer score is " + computerScore + ".")
+    } else if (computerSelection === "rock" && humanSelection === "scissors") {
+        console.log("You have selected " + humanSelection + ". Computer has selected " + computerSelection + ".")
+        console.log("No luck! Computer wins this round!")
+        console.log("Your score is " + humanScore + ".")
+        console.log("Computer score is " + ++computerScore + ".")
+    } else if (computerSelection === "paper" && humanSelection === "rock") {
+        console.log("You have selected " + humanSelection + ". Computer has selected " + computerSelection + ".")
+        console.log("No luck! Computer wins this round!")
+        console.log("Your score is " + humanScore + ".")
+        console.log("Computer score is " + ++computerScore + ".")
+    } else if (computerSelection === "scissors" && humanSelection === "paper") {
+        console.log("You have selected " + humanSelection + ". Computer has selected " + computerSelection + ".")
+        console.log("No luck! Computer wins this round!")
+        console.log("Your score is " + humanScore + ".")
+        console.log("Computer score is " + ++computerScore + ".")
+    } else {
+        console.log("You have selected " + humanSelection + ". Computer has selected " + computerSelection + ".")
+        console.log("It's a tie! Score remains the same.")
+    }
 }
+
+playRound()
+
