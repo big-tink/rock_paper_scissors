da47ac3fe308418f7f63ffc8102e947d36b56403
Jake Bellm <[jbsnake13@gmail.com]>
2024-08-12 10:21:30 -0500

    Create functions for user and comp choice

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,24 @@
 console.log("Hello World!")
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let compResult = Math.floor(Math.random() * 3) + 1;
+        if (compResult === 1) {
+            compResult = "rock"
+        } else if (compResult === 2) {
+            compResult = "paper"
+        } else {
+            compResult = "scissors"
+        }
+}
+
+function getHumanChoice() {
+    let userChoice = prompt("Please type rock, paper or scissors:");
+}
+
+function playGame() {
+    getHumanChoice();
+    getComputerChoice;
+}
