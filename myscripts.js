f5363ba4e503486dabf7238d84e641a5b6f9b688
Jake Bellm <[jbsnake13@gmail.com]>
2024-08-18 13:42:28 -0500

    Added results and winner

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,9 @@
 
 let humanScore = 0;
 let computerScore = 0;
+let humanGameSet = 0;
+let computerGameSet = 0;
+
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -35,14 +38,44 @@ function playGame(humanSelection) {
             results.textContent = "It's a tie!"
     }
 
-    console.log(gameResult, humanScore, computerScore)
+    if (humanScore === 5) {
+        humanScore = 0;
+        computerScore = 0;
+        ++humanGameSet;
+        results.textContent = "You have won the set!"
+    } else if (computerScore === 5) {
+        computerScore = 0;
+        humanScore = 0;
+        ++computerGameSet;
+        results.textContent = "Computer has won the set!"
+    }
+
+    if (humanGameSet === 5) {
+        humanGameSet = 0;
+        computerGameSet = 0;
+        results.textContent = "Congratulations! You have won!"
+    } else if (computerGameSet === 5) {
+        humanGameSet = 0;
+        computerGameSet = 0;
+        results.textContent = "No luck this time! Try again!"
+    }
+
+    console.log(gameResult, humanScore, computerScore, humanGameSet, computerGameSet);
+    currentGameScore.textContent = 'Current Score:\nPlayer: ' + humanScore + '\nComputer: ' + computerScore;
+    currentSetScore.textContent = 'Current Set Score:\nPlayer: ' + humanGameSet +'\nComputer: ' + computerGameSet;
+    return (humanScore, computerScore, humanGameSet, computerGameSet)
 }
 
 const currentGame = document.querySelector(".currentGame");
 const results = document.createElement("div");
+results.setAttribute("style", "font-size: 25px");
 currentGame.appendChild(results);
 
-
+const overallResults = document.querySelector(".resultsContainer");
+const currentGameScore = document.createElement("div");
+const currentSetScore = document.createElement("div");
+overallResults.appendChild(currentGameScore);
+overallResults.appendChild(currentSetScore);
 
 const btnRock = document.querySelector("button.rock");
 btnRock.addEventListener("click", () => {
@@ -58,4 +91,3 @@ const btnScissors = document.querySelector("button.scissors");
 btnScissors.addEventListener("click", () => {
     playGame("scissors");
 });
-
